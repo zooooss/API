1341071cbfd5f721201e8cfcb1dc93ee0a39b524
JIWOO KWON <wldn000829@gmail.com>
2025-09-26 11:33:11 +0900

    Drawing Bitmap

---
+++ Bitmap/bitmap.cpp
@@ -0,0 +1,67 @@
+#include <windows.h>
+#include <tchar.h>
+#include "resource.h"
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+HINSTANCE g_hInst;
+HWND hWndMain;
+LPCTSTR lpszClass = _T("Bitmap");
+
+int APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPSTR lpszCmdParam, _In_ int nCmdShow)
+{
+	HWND hWnd;
+	MSG Message;
+	WNDCLASS WndClass;
+	g_hInst = hInstance;
+
+	WndClass.cbClsExtra = 0;
+	WndClass.cbWndExtra = 0;
+	WndClass.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	WndClass.hInstance = hInstance;
+	WndClass.lpfnWndProc = WndProc;
+	WndClass.lpszClassName = lpszClass;
+	WndClass.lpszMenuName = NULL;
+	WndClass.style = CS_HREDRAW | CS_VREDRAW;
+	RegisterClass(&WndClass);
+
+	hWnd = CreateWindow(lpszClass, lpszClass, WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+		NULL, (HMENU)NULL, hInstance, NULL);
+	ShowWindow(hWnd, nCmdShow);
+	hWndMain = hWnd; // Store hWnd information in a global variable hWndMain!
+
+	while (GetMessage(&Message, NULL, 0, 0)) {
+		TranslateMessage(&Message);
+		DispatchMessage(&Message);
+	}
+	return (int)Message.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage,
+	WPARAM wParam, LPARAM lParam)
+{
+	HDC hdc,MemDC;
+	PAINTSTRUCT ps;
+	HBITMAP MyBitmap, OldBitmap;
+
+	switch (iMessage) {
+	case WM_PAINT:
+		hdc = BeginPaint(hWnd, &ps);
+		MemDC = CreateCompatibleDC(hdc);
+		MyBitmap = LoadBitmap(g_hInst, MAKEINTRESOURCE(IDB_BITMAP1));
+		OldBitmap = (HBITMAP)SelectObject(MemDC, MyBitmap);
+		BitBlt(hdc, 0, 0, 123, 160, MemDC, 0, 0, SRCCOPY);
+		//StretchBlt(hdc, 50, 0, 296, 320, MemDC, 0, 0, 123, 160, SRCCOPY);
+		SelectObject(MemDC, OldBitmap);
+		DeleteObject(MyBitmap); 
+		EndPaint(hWnd, &ps);
+		return 0;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+	return(DefWindowProc(hWnd, iMessage, wParam, lParam));
+}
